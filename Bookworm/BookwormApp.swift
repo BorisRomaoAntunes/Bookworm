1bacfaf98bdfd6481a1c1fa17a450141583cb023
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-05-29 18:35:07 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Boris R on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
